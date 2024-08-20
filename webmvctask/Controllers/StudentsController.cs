96d9109a0cf139851e43c290f170e5d8cbd35457
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2024-08-20 11:10:24 +0300

    Add project files.

---
+++ webmvctask/Controllers/StudentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webmvctask.Controllers
+{
+    public class StudentsController : Controller
+    {
+      
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+        public IActionResult GetId(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
